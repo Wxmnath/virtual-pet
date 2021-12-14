701391edb4d4b9fa6df97712aa44aba85885836f
Wxmnath <Wxmnath@aol.com>
2021-12-14 21:09:00 +0000

    Getting older added

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -10,3 +10,15 @@ it("sets the name property", () => {
 
   expect(pet.name).toEqual("Fido");
 });
+it("has a initial age of 0", () => {
+  const pet = new Pet("Fido");
+
+  expect(pet.age).toEqual(0);
+});
+it("increments the age by 1", () => {
+  const pet = new Pet("Fido");
+
+  pet.growUp();
+
+  expect(pet.age).toEqual(1);
+});
